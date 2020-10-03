13bf670c231015e64f9a7fad62eef1fdeae59081
Matheus Henrique <idoctorrini@gmail.com>
2020-10-03 12:31:24 -0300

    First commir
    First commit

---
+++ JS/main.js
@@ -0,0 +1,82 @@
+
+
+
+function Botao(){
+    document.getElementById("clicou").innerHTML = "<b>Bom trabalho</b>";
+    //alert("Obrigado por clicar");
+}
+
+function ProximaPagina(){
+    window.open("https://www.google.com/webhp?hl=pt-BR&sa=X&ved=0ahUKEwim1bXT2ZjsAhU5HLkGHQ5VB24QPAgI");
+}
+
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+alert(soma(5,6));
+alert(setReplace("japao e bao", "japao", "teste"))
+*/
+
+/*
+var count = 0;
+for(count; count <=5; count++){
+    console.log(count);
+}
+*/
+
+/*
+var count = 0;
+
+while (count < 5){
+    console.log(count);
+    count = count + 1;
+}
+*/
+
+/*
+
+var idade = 13;
+
+if(idade >= 18){
+    alert("maior de idade");
+}
+else{
+    alert("menor de idade");
+}
+*/
+
+
+//var frute =[{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxo"}]
+
+//var frute = {nome: "maçã", cor: "vermelha", peso: 10}
+
+//console.log(frute);
+
+
+//var lista = ["maçã", "pera", "laranja"];
+
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista.toString());
+
+/*
+var nome = "Matheus Ferreira";
+var idade = 21;
+var idade2 = 1;
+
+alert(nome + " tem " + (idade + idade2) + " anos");
+console.log(nome);
+console.log(idade);
+
+*/
+
